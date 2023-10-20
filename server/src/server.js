edc239e7e1e7e0bfc93416e4cb5021236206ff68
Andrew Pinon <andrewpinon@Andrews-MacBook-Pro.local>
2023-10-21 05:32:30 +0800

    new deployment environment

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { main } from "../..";
+import { main } from "../../index.js";
 
 
 const app = express();
